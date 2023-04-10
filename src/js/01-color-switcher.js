a8386bd460801f466a195876cdcdaa4c6ed23da1
NataliiaSav <savchenko6561@gmail.com>
2023-04-10 09:54:34 +0300

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.setAttribute('disabled', 'disabled');
+  buttonStop.removeAttribute('disabled');
+});
+
+// startBtn.addEventListener('click', () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.removeAttribute('disabled');
+});
