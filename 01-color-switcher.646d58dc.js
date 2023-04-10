14f328f0c160c29a3f1ebcce5ab43aeca0075dfe
NataliiaSav <116449758+NataliiaSav@users.noreply.github.com>
2023-04-10 06:55:03 +0000

    Deploying to gh-pages from @ NataliiaSav/goit-js-hw-09@a8386bd460801f466a195876cdcdaa4c6ed23da1 🚀

---
+++ 01-color-switcher.646d58dc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;e.addEventListener("click",(()=>{d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled","disabled"),t.removeAttribute("disabled")})),t.addEventListener("click",(()=>{clearInterval(d),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.646d58dc.js.map
