6c16ea76ab142d8e377d577f323df385be3c1e80
NataliiaSav <savchenko6561@gmail.com>
2023-04-14 16:58:38 +0300

    update task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
-const delay = document.querySelector('[name="delay"]');
+const delayFirst = document.querySelector('[name="delay"]');
 const step = document.querySelector('[name="step"]');
 const amount = document.querySelector('[name="amount"]');
 
@@ -10,14 +10,24 @@ form.addEventListener('click', getPromises);
 function getPromises(event) {
   event.preventDefault();
 
-  let delayEl = delay.value;
-  let stepEl = step.value;
-  let amountEl = amount.value;
+  const delayEl = Number(delayFirst.value);
+  const stepEl = Number(step.value);
+  const amountEl = Number(amount.value);
+
+  for (let position = 1, i = 0; position <= amountEl; position += 1, i += 1) {
+    let delay = delayEl + stepEl * i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
